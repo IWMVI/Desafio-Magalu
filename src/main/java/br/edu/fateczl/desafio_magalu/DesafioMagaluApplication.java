45efca8f312ed58588c861e9198895e5eeb2c668
Wallace Martins <wmvwallace@gmail.com>
2024-10-21 13:20:30 -0300

    Criação do projeto.

---
+++ src/main/java/br/edu/fateczl/desafio_magalu/DesafioMagaluApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fateczl.desafio_magalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMagaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMagaluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fateczl/desafio_magalu/DesafioMagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fateczl.desafio_magalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMagaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
